0eba48888fd7f6df430f27c86c6c29cd7353c598
Fabio Plunser <fabio.plunser@student.uibk.ac.at>
2025-02-26 17:12:03 +0100

    Basic implementation

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,37 +1,94 @@
+use enigo::{Enigo, Mouse, Settings};
+use serde::Serialize;
+use tauri::AppHandle;
+use tauri::Emitter;
+use tauri::Manager;
+use window_vibrancy::*;
+
+#[derive(Debug, Clone, Serialize)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+fn show_clipboard(app: AppHandle) {
+    println!("Ctrl+Shift+C Pressed!");
+    let window = app.get_webview_window("main").unwrap();
+    let enigo = Enigo::new(&Settings::default()).unwrap();
+    let mouse_location = enigo.location();
+    match mouse_location {
+        Ok((x, y)) => {
+            app.emit("show_clipboard", Position { x, y }).unwrap();
+            println!("{:?}", mouse_location);
+        }
+        Err(e) => {
+            println!("Mouse location not found: {:?}", e);
+        }
+    }
+    window.show().unwrap();
+    window.set_focus().unwrap();
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
-        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_positioner::init())
         .setup(|app| {
             #[cfg(desktop)]
             {
-                let ctrl_n_shortcut = Shortcut::new(Some(Modifiers::CONTROL), Code::KeyN);
-                app.handle().plugin(
-                    tauri_plugin_global_shortcut::Builder::new()
-                        .with_handler(move |_app, shortcut, event| {
-                            println!("{:?}", shortcut);
-                            if shortcut == &ctrl_n_shortcut {
-                                match event.state() {
-                                    ShortcutState::Pressed => {
-                                        println!("Ctrl-N Pressed!");
-                                    }
-                                    ShortcutState::Released => {
-                                        println!("Ctrl-N Released!");
-                                    }
+                use tauri_plugin_global_shortcut::{
+                    Code, GlobalShortcutExt, Modifiers, Shortcut, ShortcutState,
+                };
+
+                let shortcut =
+                    Shortcut::new(Some(Modifiers::CONTROL | Modifiers::SHIFT), Code::KeyC);
+
+                let plugin = tauri_plugin_global_shortcut::Builder::new()
+                    .with_handler(move |app: &AppHandle, current_shortcut, event| {
+                        if current_shortcut == &shortcut {
+                            match event.state() {
+                                ShortcutState::Pressed => {
+                                    show_clipboard(app.clone());
+                                }
+                                ShortcutState::Released => {
+                                    println!("Ctrl+Shift+C Released!");
                                 }
                             }
-                        })
-                        .build(),
-                )?;
+                        }
+                    })
+                    .build();
 
-                app.global_shortcut().register(ctrl_n_shortcut)?;
+                app.handle().plugin(plugin)?;
+                app.handle().plugin(tauri_plugin_positioner::init())?;
+
+                app.global_shortcut().register(shortcut.clone()).unwrap();
+
+                let window = app.get_webview_window("main").unwrap();
+
+                #[cfg(target_os = "macos")]
+                apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
+                    .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+                #[cfg(target_os = "windows")]
+                apply_acrylic(&window, Some((18, 18, 18, 125)))
+                    .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+
+                window.hide().unwrap();
+
+                // window.clone().on_window_event(move |event| {
+                //     if let tauri::WindowEvent::Focused(is_focused) = event {
+                //         if !is_focused {
+                //             window.hide().unwrap();
+                //         }
+                //     }
+                // });
             }
             Ok(())
         })
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,83 @@
 <script lang="ts">
-  
+  import { listen } from "@tauri-apps/api/event";
+  import { getCurrentWindow, PhysicalPosition } from "@tauri-apps/api/window";
+  import { writeText, readText } from "@tauri-apps/plugin-clipboard-manager";
+
+  listen("show_clipboard", async (event) => {
+    console.log("event happened");
+    console.log("show_clipboard", event.payload);
+    let x = event.payload.x;
+    let y = event.payload.y;
+    const window = getCurrentWindow();
+    await window.setPosition(new PhysicalPosition(x, y));
+  });
+
+  let tests = $state([{ name: "test1" }, { name: "test2" }, { name: "test3" }]);
+  let search = $state("");
+
+  let filteredTests = $derived.by(() => {
+    return tests
+      .map((test) => ({
+        ...test,
+        match: test.name.toLowerCase().includes(search.toLowerCase()),
+      }))
+      .sort((a, b) => {
+        if (a.match && !b.match) return -1; // Move matching items to the top
+        if (!a.match && b.match) return 1;
+        return 0; // Keep the original order for non-matching items
+      });
+  });
+
+  async function handleEntry(test) {
+    // const window = getCurrentWindow();
+    // await window.hide();
+
+    await copyTextToClipboard(test.name);
+  }
+  $inspect(filteredTests);
+
+  async function copyTextToClipboard(text: string) {
+    await writeText(text);
+  }
 </script>
 
-<div></div>
+<div class="h-screen w-full">
+  <div class="w-full">
+    <label
+      class="input w-full bg-gray-300/30 border-0 outline-0 focus-within:outline-none rounded-b-none"
+    >
+      <svg
+        class="h-[1em] opacity-50"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        ><g
+          stroke-linejoin="round"
+          stroke-linecap="round"
+          stroke-width="2.5"
+          fill="none"
+          stroke="currentColor"
+          ><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"
+          ></path></g
+        ></svg
+      >
+      <input
+        bind:value={search}
+        type="search"
+        class="grow bg-transparent text-white font-bold outline-hidden focus:outline-hidden rounded-b-none"
+        placeholder="Search"
+      />
+    </label>
+    <div class="pt-2">
+      <ul class="w-full text-white">
+        {#each filteredTests as test}
+          <li class="px-2 w-full py-1">
+            <button
+              class="hover:bg-gray-500/30 bg-gray-800/20 p-2 rounded-md w-full text-left cursor-pointer transform transition duration-200 active:scale-90"
+              onclick={async () => handleEntry(test)}>{test.name}</button
+            >
+          </li>
+        {/each}
+      </ul>
+    </div>
+  </div>
+</div>
